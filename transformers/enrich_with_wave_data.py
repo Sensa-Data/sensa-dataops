3141fcc03a255a1e19a19c6b1f46aea44a179f52
abdullah-cefalo <abdullah@Sensa.no>
2024-10-04 11:07:37 +0000

    sdm-65: make enrich_with_wave_data async

--- transformers/enrich_with_wave_data.py
+++ transformers/enrich_with_wave_data.py
@@ -1,44 +1,68 @@
-if 'transformer' not in globals():
+if "transformer" not in globals():
     from mage_ai.data_preparation.decorators import transformer
-if 'test' not in globals():
+if "test" not in globals():
     from mage_ai.data_preparation.decorators import test
-
-import requests
 import pandas as pd
-import json
+import asyncio
+import httpx
+import nest_asyncio
 
+nest_asyncio.apply()
+
+
+async def get_wave_data(row: dict, headers: dict) -> str:
+    latitude = row["latitude"]
+    longitude = row["longitude"]
+    url = f"https://www.barentswatch.no/bwapi/v1/waveforecastpoint/nearest?x={longitude}&y={latitude}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        if response.status_code == 200:
+            response = response.json()
+            wave_data = response["totalSignificantWaveHeight"]
+        else:
+            wave_data = None
+    return wave_data
+
+
+async def process_df(df: pd.DataFrame, headers):
+    wave_col = []
+    # chunks = [df[i : i + 1000] for i in range(0, len(df), 1000)]
+    chunks = [df[i : i + 1000] for i in range(0, len(df), 1000)]
+    for chunk in chunks:
+
+        tasks = [
+            get_wave_data(row, headers) for index, row in chunk.iterrows()
+        ]  # Create a list of tasks
+        results = await asyncio.gather(*tasks)  # Await all tasks concurrently
+        for result in results:
+            wave_col.append(result)
 
 
 @transformer
-def transform(token,data, *args, **kwargs):
+def transform(token: dict, df: pd.DataFrame, *args, **kwargs):
     """
-    ...
+    Block to enrich ais ship data with wave height data
+    Args:
+        token: Token for Barents Watch general API
+        df: Data frame with ais data to be enriched with wave data
+    Returns:
+        Input data frame enriched with wave data
     """
-    print (token)
-    df = data
-    df = df.head(10)
-    token = token['get_bw_api_access_token'][0]
+    token = token["get_bw_api_access_token"]
+    token = token[0]
     headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Authorization": f"Bearer {token}"
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Bearer {token}",
     }
-    respons_code = []
-    wave_col=[]
-    for index, row in df.iterrows():
-        latitude= row['latitude']
-        longitude= row['longitude']
-        #url2 = 'https://www.barentswatch.no/bwapi/v1/waveforecastpoint/nearest?x=5.736098&y=60.073905'
-        url = f"https://www.barentswatch.no/bwapi/v1/waveforecastpoint/nearest?x={longitude}&y={latitude}"
-        response = requests.get(url,headers=headers)
-        if response.status_code == 200:
-            response = response.json()
-            wave_data = response['totalSignificantWaveHeight']
-        else:
-            wave_data = None
-        wave_col.append(wave_data)
-
-    df['Wave_height']= wave_col
-
+    #
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:  # no event loop running:
+        loop = asyncio.new_event_loop()
+    result = loop.run_until_complete(
+        process_df(df, headers)
+    )  # Run async function until complete
+    df["Wave_height"] = result
     return df
 
 
@@ -47,4 +71,4 @@ def test_output(output, *args) -> None:
     """
     Template code for testing the output of the block.
     """
-    assert output is not None, 'The output is undefined'
+    assert output is not None, "The output is undefined"
